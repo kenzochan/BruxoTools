1d1fa8f1a510470f0f076afae227b12fb6e3dc66
Daniel Kenzo <103758337+kenzochan@users.noreply.github.com>
2025-02-16 16:22:35 -0300

    4 commit

--- obj/Debug/net9.0/BruxoTools.AssemblyInfo.cs
+++ obj/Debug/net9.0/BruxoTools.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BruxoTools")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcf4357bbfd4fce9c4f71ad06f4c410246d9ce0b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f73905c6a40e54e9d2e5a991de229b61bc5528da")]
 [assembly: System.Reflection.AssemblyProductAttribute("BruxoTools")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BruxoTools")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
